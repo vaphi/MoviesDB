ef7233cb0ce88c40e42e36ac7ae1842886bd5033
VaPhi <vphi@uncc.edu>
2019-06-23 21:23:28 -0400

    heroku login test

---
+++ client/build/precache-manifest.bb98ad0828d0a84148347674f5c3054c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6ed7f18eb7033f42249",
+    "url": "/static/css/main.c14ec72d.chunk.css"
+  },
+  {
+    "revision": "d6ed7f18eb7033f42249",
+    "url": "/static/js/main.01b74f84.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5f16bf62a34222c5bdff",
+    "url": "/static/js/2.2cdb8c7f.chunk.js"
+  },
+  {
+    "revision": "b7a8860eb374b39197f24002de3d7a2d",
+    "url": "/index.html"
+  }
+];
